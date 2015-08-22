244cf961e60c2aef8ea6a8f19f26ed723d25cd6c
Jeff Donovan <jeff.donovan@udacity.com>
2015-08-21 18:34:29 -0700

    Update fbconnect() to work for FB API v2.4
    API calls are made using v2.4 and fbconnect() now explicitly requests name, id and email.

--- Lesson4/step2/project.py
+++ Lesson4/step2/project.py
@@ -56,12 +56,12 @@ def fbconnect():
     result = h.request(url, 'GET')[1]
 
     # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.2/me"
+    userinfo_url = "https://graph.facebook.com/v2.4/me"
     # strip expire tag from access token
     token = result.split("&")[0]
 
 
-    url = 'https://graph.facebook.com/v2.2/me?%s' % token
+    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     # print "url sent for API access:%s"% url
@@ -77,7 +77,7 @@ def fbconnect():
     login_session['access_token'] = stored_token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.2/me/picture?%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
