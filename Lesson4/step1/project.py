6e6aa1a3c047f21767060ebe45d39ab26d599b74
Adarsh Nair <adarsh108@gmail.com>
2016-03-17 10:20:44 -0700

    Update project.py
    login_session['access_token'] = credentials.access_token added in

--- Lesson4/step1/project.py
+++ Lesson4/step1/project.py
@@ -94,7 +94,7 @@ def gconnect():
         return response
 
     # Store the access token in the session for later use.
-    login_session['credentials'] = credentials
+    login_session['access_token'] = credentials.access_token
     login_session['gplus_id'] = gplus_id
 
     # Get user info
