97ece94393a3e4d9e84fb35d9ef4f60078d400d9
Steven Wooding <steve@stevenwooding.com>
2017-04-06 16:52:08 +0100

    fix: Correctly process Facebook OAuth JSON response
    For version 2.3 of the Graph API, the response when exchanging
    the short-lived token for the long-lived token is now in JSON format.
    This change takes that into account. It also explicitly defines the API
    version, rather than leaving it to default to the oldest available. It was
    this that previous left the error undetected.

--- Lesson4/step2/project.py
+++ Lesson4/step2/project.py
@@ -50,16 +50,14 @@ def fbconnect():
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+    url = 'https://graph.facebook.com/v2.4/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
+    data = json.loads(result)
 
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me"
-    # strip expire tag from access token
-    token = result.split("&")[0]
-
+    # Extract the access token from response
+    token = 'access_token=' + data['access_token']
 
     url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
     h = httplib2.Http()
