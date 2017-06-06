21145cc1fbbd2e73a2ec99e3f12c9ca066091454
Cristian Thompson <Thompsonacristian@gmail.com>
2017-06-06 17:03:41 -0300

    Add multi-line comment
    Outlining the process of dissecting out the specific server access token

--- Lesson4/step2/project.py
+++ Lesson4/step2/project.py
@@ -59,7 +59,13 @@ def fbconnect():
 
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.8/me"
-    # strip expire tag from access token
+    ''' 
+        Due to the formatting for the result from the server token exchange we have to 
+        split the token first on commas and select the first index which gives us the key : value 
+        for the server access token then we split it on colons to pull out the actual token value
+        and replace the remaining quotes with nothing so that it can be used directly in the graph
+        api calls
+    '''
     token = result.split(',')[0].split(':')[1].replace('"', '')
 
     url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
