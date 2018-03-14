3498b6d7ff9d141c2b46f85881c809e4d5c890b4
Vishwas_C <vishwas-c@users.noreply.github.com>
2018-03-14 20:08:33 +0530

    Added import string

--- Lesson2/step7/project.py
+++ Lesson2/step7/project.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 from flask import session as login_session
-import random
+import random,string
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
